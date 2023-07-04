81e33158e7f48f0632e946b4e9ba2abdd5077f6d
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-07-04 13:48:45 +1000

    fixed initial database setup

--- postgres/docker-entrypoint-initdb.d/create-tables.sql
+++ postgres/docker-entrypoint-initdb.d/create-tables.sql
@@ -1,14 +1,23 @@
-CREATE TABLE IF NOT EXISTS blogs (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL,
-    likes INTEGER NOT NULL
+
+CREATE TABLE IF NOT EXISTS "users" (
+    "id"  SERIAL PRIMARY KEY, 
+    "name" VARCHAR(128) NOT NULL, 
+    "username" VARCHAR(128) NOT NULL UNIQUE, 
+    "hashed_password" VARCHAR(128) NOT NULL
 );
 
-INSERT INTO blogs (title, author, url, likes) VALUES ('React patterns', 'Michael Chan', 'https://reactpatterns.com/', 7);
-INSERT INTO blogs (title, author, url, likes) VALUES ('Go To Statement Considered Harmful', 'Edsger W. Dijkstra', 'https://www.mindspring.com/~mgrand/palace/hoax.html', 5);
-INSERT INTO blogs (title, author, url, likes) VALUES ('Canonical string reduction', 'Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', 12);
-INSERT INTO blogs (title, author, url, likes) VALUES ('First class tests', 'Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', 10);
-INSERT INTO blogs (title, author, url, likes) VALUES ('TDD harms architecture', 'Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', 0);
-INSERT INTO blogs (title, author, url, likes) VALUES ('Type wars', 'Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', 2);
+/* password = letmein */
+INSERT INTO "users" ("id","name","username","hashed_password") 
+VALUES (DEFAULT,'Admin','admin','$2b$10$5EguhOmNO6hE4m11nenxkOncukTWmf/aEFcFm9pvENUZhberk1ZLe');
+
+CREATE TABLE IF NOT EXISTS blogs (
+    id  SERIAL PRIMARY KEY, 
+    title VARCHAR(128) NOT NULL, 
+    author VARCHAR(128) NOT NULL, 
+    url VARCHAR(128) NOT NULL, 
+    likes INTEGER NOT NULL, 
+    owner_id INTEGER REFERENCES users (id)
+);
+
+INSERT INTO blogs (title, author, url, likes, owner_id) VALUES ('React patterns', 'Michael Chan', 'https://reactpatterns.com/', 7, 1);
+INSERT INTO blogs (title, author, url, likes, owner_id) VALUES ('Go To Statement Considered Harmful', 'Edsger W. Dijkstra', 'https://www.mindspring.com/~mgrand/palace/hoax.html', 5, 1);
