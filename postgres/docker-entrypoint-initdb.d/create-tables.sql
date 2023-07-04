3206433bfffc6f856d66ecaf427f1cf58e7454a8
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-07-04 21:26:54 +1000

    added createAt and updatedAt

--- postgres/docker-entrypoint-initdb.d/create-tables.sql
+++ postgres/docker-entrypoint-initdb.d/create-tables.sql
@@ -1,13 +1,15 @@
 
-CREATE TABLE IF NOT EXISTS "users" (
-    "id"  SERIAL PRIMARY KEY, 
-    "name" VARCHAR(128) NOT NULL, 
-    "username" VARCHAR(128) NOT NULL UNIQUE, 
-    "hashed_password" VARCHAR(128) NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+    id  SERIAL PRIMARY KEY, 
+    name VARCHAR(128) NOT NULL, 
+    username VARCHAR(128) NOT NULL UNIQUE, 
+    hashed_password VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
 
 /* password = letmein */
-INSERT INTO "users" ("id","name","username","hashed_password") 
+INSERT INTO users (id,name,username,hashed_password) 
 VALUES (DEFAULT,'Admin','admin','$2b$10$5EguhOmNO6hE4m11nenxkOncukTWmf/aEFcFm9pvENUZhberk1ZLe');
 
 CREATE TABLE IF NOT EXISTS blogs (
@@ -16,8 +18,16 @@ CREATE TABLE IF NOT EXISTS blogs (
     author VARCHAR(128) NOT NULL, 
     url VARCHAR(128) NOT NULL, 
     likes INTEGER NOT NULL, 
-    owner_id INTEGER REFERENCES users (id)
+    owner_id INTEGER REFERENCES users (id),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO blogs (title, author, url, likes, owner_id) VALUES ('React patterns', 'Michael Chan', 'https://reactpatterns.com/', 7, 1);
-INSERT INTO blogs (title, author, url, likes, owner_id) VALUES ('Go To Statement Considered Harmful', 'Edsger W. Dijkstra', 'https://www.mindspring.com/~mgrand/palace/hoax.html', 5, 1);
+INSERT INTO blogs (title, author, url, likes, owner_id)
+VALUES ('React patterns', 'Michael Chan', 'https://reactpatterns.com/', 7, 1);
+
+INSERT INTO blogs (title, author, url, likes, owner_id)
+VALUES ('Kent C. Dodds’s Blog', 'Kent C. Dodds', 'https://kentcdodds.com/blog', 5, 1);
+
+INSERT INTO blogs (title, author, url, likes, owner_id)
+VALUES ('Robin Wieruch’s Blog', 'Robin Wieruch', 'https://www.robinwieruch.de/blog/', 3, 1);
